e635012f3b91b9704e742bd0862b8d384cce28b0
Tim White <tim@whiteitsolutions.com.au>
2016-01-26 20:32:32 +1000

    Default route redirect to groups page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -22,10 +22,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->redirectToRoute('groups');
     }
 
     /**
